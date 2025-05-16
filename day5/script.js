342d3f8a751c234cdaa1dbabc850ea98b7937dca
Omid908 <omidnikzad08@gmail.com>
2025-05-16 07:33:26 +0600

    daily practice

--- day5/script.js
+++ day5/script.js
@@ -1,20 +1,3 @@
-const calcAverage = ( a , b , c ) => (( a + b + c ) / 3);
-let scoreDolphins = calcAverage (99,99,99);
-let scorekoalas = calcAverage (85,11,11);
-function checkWinner ( avgDolphins , avgKoalas ){
-    if ( avgDolphins >= 2 * avgKoalas){
-        console.log(`Dolphins win ${avgDolphins} vs. ${avgKoalas}`);
-    }
-    else if ( avgKoalas >= 2 * avgDolphins){
-        console.log(`Koalas win ${avgKoalas} vs. ${avgDolphins}`);
-    }
-    else{
-        console.log("No team wins...");
-    }
-}
-checkWinner ( scoreDolphins , scoreKoalas );
-
-/*
 const calcAverage = ( a , b , c ) => Number((( a + b + c ) / 3).toFixed(2));
 let avgDolphins = calcAverage (99,99,99);
 let avgKoalas = calcAverage (85,11,11);
@@ -30,4 +13,11 @@ function checkWinner ( avgDolphins , avgKoalas ){
     }
 }
 checkWinner ( avgDolphins , avgKoalas );
-*/
+
+const calcTip = bills => ( bills <= 300 && bills >= 50 ) ? bills * 0.15 : bills * 0.20;
+const bills = [ 125 , 555 , 44 ];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [bills[0] + tips[0] , bills[1] + tips[1] , bills [2] + tips[2]];
+console.log("Bills: ", bills);
+console.log("Tips: ", tips);
+console.log("Total", total);
---
+++ day6/script.js
@@ -0,0 +1,66 @@
+/*
+const ahmad = {
+    firstname : "Ahmad",
+    lastname : "Dastpak",
+    age : "24",
+    job : "teacher",
+    location: "US"
+}
+
+const namekey = 'name';
+console.log(ahmad['first' + namekey]);
+console.log(ahmad['last' + namekey]);
+console.log(ahmad.age);
+
+const intrestedin = prompt ( "What do you want to know about this person ( firstname, lastname, age, job, location? ");
+if(ahmad[intrestedin]){
+    console.log(ahmad[intrestedin]);
+} else{
+    console.log("Property doesn't exist");
+}
+ahmad.friends = ["Ali", "Mohammad", "Ehsan"];
+console.log(`${ahmad.firstname} has ${ahmad.friends.length} friends and his best friend is ${ahmad.friends[0]}`);
+
+const ahmad = {
+    firstname : "Ahmad",
+    lastname : "Dastpak",
+    birthyear : 2004,
+    job : "teacher",
+    driverLicense : false,
+    location: "US",
+    calcAge : function(){
+        this.age = 2025 - this.birthyear;
+        return this.age
+    },
+    getSummary : function(){
+        console.log(`${this.firstname} is ${this.age} years old, he is a ${this.job} and he has ${this.driverLicense ? 'a': 'no'} driving license.`)
+    }
+};
+console.log(ahmad.calcAge());
+console.log(ahmad.age);
+console.log(ahmad.getSummary());
+*/
+/* Write your code below. Good luck! 🙂 */
+const mark = {
+    fullName : 'Mark Miller',
+    mass : 78,
+    height : 1.69,
+    calcBMI : function(){
+        this.bmi =  this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+};
+const john = {
+    fullName: 'John Smith',
+    mass: 92,
+    height: 1.95,
+    calcBMI : function(){
+        this.bmi =  this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+};
+if( john.calcBMI() > mark.calcBMI()){
+    console.log(`${john.fullName}'s BMI ${john.bmi} is higher than Mark Miller's ${mark.bmi}`);
+} else{
+    console.log(`${mark.fullName}'s BMI ${mark.bmi} is higher than John Smith's ${john.bmi}`);
+}
