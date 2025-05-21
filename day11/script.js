0f82dbbd8d3cd0d7568157710aa08527c4f74264
Omid908 <omidnikzad08@gmail.com>
2025-05-21 19:06:00 +0600

    daily practice

--- day11/script.js
+++ day11/script.js
@@ -66,7 +66,57 @@
 // updateUI();
 
 //------------------- Random number generator in range --------------------
-let max = 100;
-let min = 50;
-const random = Math.trunc(Math.random() * (max - min)) + min;
-document.getElementById("countlabel").textContent = random;
+// let max = 100;
+// let min = 50;
+// const random = Math.trunc(Math.random() * (max - min)) + min;
+// document.getElementById("countlabel").textContent = random;
+
+//------------------- Ternary Operator --------------------
+// let student = true;
+// let message = student ? "You are a student" : "You are not a student";
+// console.log(message);
+// let purchaseamount = 125;
+// let discont = purchaseamount > 100 ? 10 : 0;
+// console.log(
+//   `Your total purchased amount is $${
+//     purchaseamount - purchaseamount * (discont / 100)
+//   }`
+// );
+//------------------- Switch --------------------
+let day = 2;
+switch (day) {
+  case 1:
+    console.log("Today is Monday");
+    break;
+  case 2:
+    console.log("Today is Tuesday");
+    break;
+}
+function getDayName(day) {
+  const days = {
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednseday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+    7: "Sunday",
+  };
+  return days[day] || "invalid input ";
+}
+console.log(`Today is ${getDayName(1)}`);
+
+let testscore = 23;
+let grade;
+switch (true) {
+  case testscore > 90:
+    grade = "A";
+    break;
+  case testscore > 80:
+    grade = "B";
+    break;
+  case testscore < 50:
+    grade = "F";
+    break;
+}
+console.log(grade);
