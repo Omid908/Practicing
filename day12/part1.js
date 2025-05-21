6bdda2c2135f770c9360464f3b2b28c83d65f416
Omid908 <omidnikzad08@gmail.com>
2025-05-22 04:55:32 +0600

    daily practice

--- day12/part1.js
+++ day12/part1.js
@@ -33,15 +33,35 @@
 //   }
 // }
 //------------------- Functions --------------------
-let isEven = function (num) {
-  return num % 2 === 0 ? "Even" : "Odd";
-};
-let add = function (a, b) {
-  return (result = a + b);
-};
-let isEmail = function (email) {
-  return email.includes("@") ? "Valid" : "Invalid";
-};
-console.log(isEmail("ahmad2@gmail.com"));
-console.log(add(3, 4));
-console.log(isEven(4));
+// let isEven = function (num) {
+//   return num % 2 === 0 ? "Even" : "Odd";
+// };
+// let add = function (a, b) {
+//   return (result = a + b);
+// };
+// let isEmail = function (email) {
+//   return email.includes("@") ? "Valid" : "Invalid";
+// };
+// console.log(isEmail("ahmad2@gmail.com"));
+// console.log(add(3, 4));
+// console.log(isEven(4));
+//------------------- Temp Conversion Program --------------------
+const textBox = document.getElementById("textBox");
+const toF = document.getElementById("toF");
+const toC = document.getElementById("toC");
+const result = document.getElementById("result");
+const btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  let temp = Number(textBox.value);
+  if (toF.checked) {
+    let far = temp * (9 / 5) + 32;
+    result.textContent = `${temp}°F in Celcius is equal to ${far.toFixed(2)}°F`;
+  } else if (toC.checked) {
+    let cel = (5 / 9) * (temp - 32);
+    result.textContent = `${temp}°C in Fahrenhiet is equal to ${cel.toFixed(
+      2
+    )}°C`;
+  } else {
+    result.textContent = "Select a Unit";
+  }
+});
