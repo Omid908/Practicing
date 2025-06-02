5aa911360f74c88b949c0216e7dcc603a7f658b2
Omid908 <omidnikzad08@gmail.com>
2025-06-02 21:09:03 +0500

    daily practice

---
+++ day21/script.js
@@ -0,0 +1,55 @@
+// let username = prompt("What's your name?");
+// alert(`Hello ${username}`);
+// let a = Number(prompt("First number?", 1));
+// let b = Number(prompt("Second number?", 2));
+
+// alert(a + b);
+// console.log(null == undefined);
+// console.log(undefined == null);
+// if ("0") {
+//   alert("Hello");
+// }
+// let question = prompt("What's the “official” name of JavaScript?");
+// let answer =
+//   question == "ECMA" ? alert("right") : alert("You don't know? ECMAscript");
+// let number = prompt("Type a number: ");
+// let result = number > 0 ? "1" : number == 0 ? "equal" : "-1";
+// alert(result);
+// let number1 = prompt("Enter 1st number : ");
+// let number2 = prompt("Enter 2nd number : ");
+// let result = Number(number2) + Number(number1) < 4 ? "below" : "over";
+// alert(result);
+// let message = prompt("Who are you? Teacher / Student");
+// let login =
+//   message === "Student"
+//     ? "Hello student"
+//     : message === "Teacher"
+//     ? "Hello teacher"
+//     : "Invalid input";
+// alert(login);
+// alert(null || 2 || undefined);
+// alert(alert(1) || 2 || alert(3));
+// alert(1 && null && 2);
+// alert(alert(1) && alert(2));
+// alert(1) && alert(2);
+// let user = prompt("Who is there? (Answer: Admin)");
+// if (!user) {
+//   alert("Canceled");
+// } else if (user === "Admin") {
+//   let pwd = prompt("Enter the password: (Answer: TheMaster)");
+
+//   if (!pwd) {
+//     alert("Canceled");
+//   } else if (pwd === "TheMaster") {
+//     alert("Welcome");
+//   } else {
+//     alert("Wrong password");
+//   }
+// } else {
+//   alert("Wrong username");
+// }
+let i = 4;
+while (i) {
+  alert(i);
+  i--;
+}
