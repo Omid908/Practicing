f33bae0552719ce86a7cff81e180ac9e40f95b55
Omid908 <omidnikzad08@gmail.com>
2025-06-03 22:05:47 +0500

    daily practice

--- day22/script.js
+++ day22/script.js
@@ -95,12 +95,71 @@
 //   console.log("Message is:", msg);
 // });
 // runCallback();
-function showprime(n) {
-  nextPrime: for (let i = 2; i < n; i++) {
-    for (let j = 2; j < i; j++) {
-      if (i % j == 0) continue nextPrime;
-    }
-    console.log(i);
+// function showprime(n) {
+//   nextPrime: for (let i = 2; i < n; i++) {
+//     for (let j = 2; j < i; j++) {
+//       if (i % j == 0) continue nextPrime;
+//     }
+//     console.log(i);
+//   }
+// }
+// showprime(12);
+// let age = prompt("Enter your age: ");
+// function checkage(age) {
+//   age > 18 ? true : confirm("Did your parents allow you?");
+// }
+// checkage();
+// let x = +prompt("Enter the number :");
+// let n = +prompt("Enter the power: ");
+// function pow(x, n) {
+//   let result = 1;
+//   for (let i = 1; i < n; i++) {
+//     result = result * x;
+//   }
+//   return result;
+// }
+// alert(`result ${pow(x, n)}`);
+// function ask(question, yes, no) {
+//   if (confirm(question) ? yes() : no());
+// }
+// function showok() {
+//   alert("You agreed");
+// }
+// function cancel() {
+//   alert("You canceled");
+// }
+// ask("Do you want to go out?", showok, cancel);
+// let age = prompt("Enter your age: ");
+// let welcome =
+//   age >= 18
+//     ? function () {
+//         alert("Greetings");
+//       }
+//     : function () {
+//         alert("Welcome");
+//       };
+// welcome();
+// function ask(question, yes, no) {
+//   if (confirm(question) ? yes() : no());
+// }
+// ask(
+//   "Do you agree?",
+//   () => alert("You agreed"),
+//   () => alert("You denied")
+// );
+function pow(x, n) {
+  let result = 1;
+
+  for (let i = 0; i < n; i++) {
+    debugger; // 🔥 This will pause execution every iteration
+    result *= x;
   }
+
+  return result;
 }
-showprime(12);
+
+let base = 2;
+let exponent = 3;
+let finalResult = pow(base, exponent);
+
+console.log("Result:", finalResult);
