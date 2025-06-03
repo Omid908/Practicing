b72fb380bf45d4507d6ff978451ca4b199b6fdc9
Omid908 <omidnikzad08@gmail.com>
2025-06-03 06:45:45 +0500

    daily practice

--- day21/script.js
+++ day21/script.js
@@ -53,8 +53,20 @@
 //   alert(i);
 //   i--;
 // }
-for (let i = 0; i < 10; i++) {
-  if (i % 2) {
-    console.log(i);
+// for (let i = 0; i < 10; i++) {
+//   if (i % 2) {
+//     // if the reminder is not zero which refers to odd numbers and it becomes true
+//     console.log(i);
+//   }
+// }
+outer: for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 3; j++) {
+    let input = prompt(`Value at coords (${i},${j})`, "");
+
+    // if an empty string or canceled, then break out of both loops
+    if (!input) break outer; // (*)
+
+    // do something with the value...
   }
 }
+alert("Done!");
---
+++ day22/script.js
@@ -0,0 +1,73 @@
+// let grid = [];
+// outer: for (let i = 0; i < 3; i++) {
+//   grid[i] = [];
+//   for (let j = 0; j < 3; j++) {
+//     let val = prompt(`[${i},${j}:Enter 'x' or 'o' or 'Exit'`);
+//     if (val === "Exit") {
+//       break outer;
+//     }
+//     if (val !== "x" && val !== "o") {
+//       alert("Invalid input");
+//       j--;
+//       continue;
+//     }
+//     grid[i][j] = val;
+//   }
+// }
+// console.table(grid);
+// let i = 0;
+// while (++i < 5) alert(i);
+// for (let i = 1; i < 10; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+// for (let i = 0; i < 3; i++) {
+//   alert(`number ${i}!`);
+// }
+// let i = 1;
+// while (i <= 3) {
+//   alert(`number ${i}`);
+//   i++;
+// }
+// let newnum = prompt("Enter a number above 100");
+// while (+newnum <= 100 && newnum != null) {
+//   newnum = prompt("Enter a number above 100");
+// }
+// alert("You entered: " + newnum);
+// let a = +prompt("Enter a value between 1 to 5");
+// switch (a) {
+//   case 1:
+//   case 2:
+//   case 3:
+//   case 4:
+//     console.log("too small");
+//     break;
+//   case 5:
+//     console.log("Exactly");
+//     break;
+//   default:
+//     console.log("invalid input");
+// }
+// function outer() {
+//   let count = 0;
+//   return function inner() {
+//     count++;
+//     console.log(count);
+//   };
+// }
+// let counter = outer();
+// counter(); // ?
+// counter(); // ?
+// function showmessage(name, message = ": hello") {
+//   name = prompt("Enter you name");
+//   name = "*" + name + "*";
+//   console.log(name + message);
+// }
+// showmessage();
+// function showcount(count) {
+//   //   alert(count ?? "No count is defined here");
+//   count = count || "empty";
+//   console.log(count);
+// }
+// showcount();
