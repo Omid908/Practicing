c5dd518f9748a0ef73217220edcabf88e075ba0f
Omid908 <omidnikzad08@gmail.com>
2025-06-15 17:19:12 +0500

    daily practice

--- day29/fp.py
+++ day29/fp.py
@@ -6,8 +6,21 @@
 # itemscopy = items[:]
 # itemscopy = sorted(itemscopy, key=str.upper)
 # print("Upper case :", itemscopy)
-name = ("Ahmad", "Mohammad", "Bilal")
-print(sorted(name))
-grades = [("Omid" , 100), ("Ali" , 85 ), ("Mohammad" , 92 )]
-grades.sort(key=lambda x:x[1] , reverse= True)
-print(grades)
+# name = ("Ahmad", "Mohammad", "Bilal")
+# print(sorted(name))
+# grades = [("Omid" , 100), ("Ali" , 85 ), ("Mohammad" , 92 )]
+# grades.sort(key=lambda x:x[1] , reverse= True)
+# print(grades)
+
+# students = {
+#     "Class" : "A",
+#     "Number" : 20,
+#     "OA/GPA" : 3.5
+# }
+# if "Class" in students :
+#     students["Class"] = "B"
+# students["Favorite teacher"] = "Ahmad"
+# print(f"{students.get("Boys", 12) } \n {students}")
+# del students["Favorite teacher"]
+# print(students.values())
+# print(len(students))
